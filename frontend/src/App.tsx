baa13e9b30a1edddc139c66062d0fd691e7798da
sigzugern <skyruden@gmail.com>
2025-09-09 20:19:32 +0200

    chakra ui setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Button, Heading, Text, Card} from "@chakra-ui/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <Card.Root size="lg" variant="elevated" p="6">
+            <Card.Body>
+                <Heading mb="4">Chakra v3 ✅</Heading>
+                <Text mb="4">Card bruker slots i v3.</Text>
+                <Button colorPalette="blue">Klikk meg</Button>
+            </Card.Body>
+        </Card.Root>
+    );
 }
-
-export default App
