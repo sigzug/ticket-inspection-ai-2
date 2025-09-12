94552649ddafb04fcd9429695ee2616b21e5e38b
sigzugern <skyruden@gmail.com>
2025-09-12 22:13:46 +0200

    finished testing notebook for creating xgboost model with optuna

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
-from django.db import models
 
-# Create your models here.
+
+# class Station(Model):
+#     name = CharField(max_length=100)
+#
+#
+# class Line(Model):
+#     name = CharField(max_length=100)
+#
+#
+# class Ride(Model):
+#     line = ForeignKey(Line, on_delete=SET_NULL)
+#     datetime = DateTimeField()
+#     departure_station = ForeignKey(Station, on_delete=SET_NULL)
+#     arrival_station = ForeignKey(Station, on_delete=SET_NULL)
+#     full = BooleanField(default=False)
+#
+#     checked = BooleanField(default=False)
+#     checked_after = ForeignKey(Station, on_delete=SET_NULL)
