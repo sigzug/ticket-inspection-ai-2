5d6ba1d83779c0677bec87f54c91178e2890d355
sigzugern <skyruden@gmail.com>
2025-09-17 20:40:06 +0200

    pydantic models for request, response and data handling

--- core/schemas.py
+++ core/schemas.py
@@ -1,16 +1,35 @@
-from typing import Literal
+from typing import Annotated
 
-from pydantic import BaseModel, ConfigDict, Field
-
-
-class ComputeRequest(BaseModel):
-    numbers: list[float] = Field(min_length=1)
-    op: Literal["sum", "avg", "max", "min"]
-
-    model_config = ConfigDict(str_strip_whitespace=True)
+from pydantic import AwareDatetime, BaseModel, Field, PositiveInt
 
 
-class ComputeResponse(BaseModel):
-    operation: str
-    count: int
-    result: float
+class ModelTimeInput(BaseModel):
+    day: Annotated[int, Field(ge=1, le=31)]
+    month: Annotated[int, Field(ge=1, le=12)]
+    year: PositiveInt
+    weekday: Annotated[int, Field(ge=0, le=6)]
+    weekofyear: Annotated[int, Field(ge=1, le=52)]
+    hour: Annotated[int, Field(ge=0, le=23)]
+    minute: Annotated[int, Field(ge=0, le=59)]
+    second: Annotated[int, Field(ge=0, le=59)]
+
+
+class ModelInput(ModelTimeInput):
+    # TODO add enums from models
+    linje: str
+    fra: str
+    til: str
+    fullt: bool
+
+
+class UseModelRequest(BaseModel):
+    # TODO add enums from models
+    linje: str
+    fra: str
+    til: str
+    fullt: bool
+    departure_time: AwareDatetime
+
+
+class UseModelResponse(BaseModel):
+    checked: bool
