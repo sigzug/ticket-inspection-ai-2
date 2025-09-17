433f4b30bbc05dacc7d863be8c74dc56bfd57fcf
sigzugern <skyruden@gmail.com>
2025-09-17 20:40:25 +0200

    missing a dependency

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from core.models import Line, Ride, Station
+
 # Register your models here.
+admin.site.register(Line)
+admin.site.register(Station)
+admin.site.register(Ride)
+
