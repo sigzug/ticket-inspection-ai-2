1ccee72fa861c31897a70601a750eced8cc85412
Sigurd Skyrud <skyruden@gmail.com>
2025-12-27 23:17:55 +0100

    delete django

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from core.models import Line, Ride, Station
-
-# Register your models here.
-admin.site.register(Line)
-admin.site.register(Station)
-admin.site.register(Ride)
-
--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import UseModelView, health
-
-urlpatterns = [
-    path("health/", health, name="health"),
-    path("use-model/", UseModelView.as_view(), name="use_model"),
-]
